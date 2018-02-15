3f9be1587a1227393843011e83d15f8807d3bdb1
Dima Gaponov <Gaponoffd@yandex.ru>
2018-02-15 17:07:28 +0300

    slider

---
+++ js/main.js
@@ -0,0 +1,63 @@
+
+  //Скролы
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        menu: "#myMenu"
+    });
+});
+
+//Слайдер
+$('.burgers__content').slick();
+
+///Мобильная версия меню
+var navMobBtnOpen = document.querySelector('.hmbrg-menu__link');
+var navMobBtnClose = document.querySelector('.close__link');
+var navMobBtn = document.querySelector('.nav-mobile');
+
+navMobBtnOpen.addEventListener('click', function(e){
+    e.preventDefault();
+    navMobBtn.classList.add('nav-mobile_activ')
+})
+
+navMobBtnClose.addEventListener('click', function(e){
+    e.preventDefault();
+    navMobBtn.classList.remove('nav-mobile_activ')
+})
+
+/////Аккордеоны 
+////team
+
+var teamName = document.querySelectorAll(".team__link");
+var teamItem = document.querySelectorAll(".team__elem");
+for (let i = 0; i < teamName.length; i++) {
+    teamName[i].addEventListener("click", function(e) {
+
+        e.preventDefault();
+
+        for (let j = 0; j < teamItem.length; j++) {
+            if (j !== i) {
+                teamItem[j].classList.remove("team__elem_activ");
+            };
+        };
+
+        this.closest('.team__elem').classList.toggle("team__elem_activ");
+    });
+};
+
+////menu
+var teamNameMenu = document.querySelectorAll(".menu__link");
+var teamItemMenu = document.querySelectorAll(".menu__elem");
+for (let l = 0; l < teamNameMenu.length; l++) {
+    teamNameMenu[l].addEventListener("click", function(e) {
+
+        e.preventDefault();
+
+        for (let y = 0; y < teamItemMenu.length; y++) {
+            if (y !== l) {
+                teamItemMenu[y].classList.remove("menu__elem_activ");
+            };
+        };
+
+        this.closest('.menu__elem').classList.toggle("menu__elem_activ");
+    });
+};
